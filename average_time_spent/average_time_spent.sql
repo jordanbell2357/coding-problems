2b5355f1b15cd8f2116386f3d2f7aba742ec67f6
Jordan Bell <jordan.bell@gmail.com>
2023-08-08 20:25:05 -0400

    four coding problems

---
+++ average_time_spent/average_time_spent.sql
@@ -0,0 +1,16 @@
+SELECT
+  DATE_FORMAT(visited_on, '%Y-%m-%d') AS visited_on,
+  FORMAT(AVG(time_spent) OVER (
+    ORDER BY visited_on
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+  ), 4) AS avg_time_spent
+FROM (
+  SELECT
+    t.visited_on,
+    AVG(t.time_spent) AS time_spent
+  FROM traffic t
+  JOIN users u ON u.id = t.user_id
+  WHERE u.user_type = 'user'
+  GROUP BY t.visited_on
+) AS subquery
+ORDER BY visited_on;
